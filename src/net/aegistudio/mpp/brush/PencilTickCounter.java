6ac1a5d2351c89b42b463ce30c46c0415cc66b9b
zhouhaha <MJX1627025175@126.com>
2016-03-14 04:22:28 +0800

    use apache-commons-math FastMath

--- src/net/aegistudio/mpp/brush/PencilTickCounter.java
+++ src/net/aegistudio/mpp/brush/PencilTickCounter.java
@@ -8,4 +8,33 @@ public class PencilTickCounter {
 		this.y = y;
 		this.count = count;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final PencilTickCounter other = (PencilTickCounter) obj;
+		if (this.x != other.x) {
+			return false;
+		}
+		if (this.y != other.y) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 61 * hash + this.x;
+		hash = 61 * hash + this.y;
+		return hash;
+	}
 }
